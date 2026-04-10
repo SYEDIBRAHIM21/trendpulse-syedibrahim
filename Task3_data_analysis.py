5caa8ab32bd8b0f1c2969df21e1a5ab604ee369c
SYEDIBRAHIM21 <127423728+SYEDIBRAHIM21@users.noreply.github.com>
2026-04-10 22:26:11 +0530

    Update Task3_data_analysis.py

--- Task3_data_analysis.py
+++ Task3_data_analysis.py
@@ -1,4 +1,24 @@
-# Task 3: Data Analysis
+import csv
 
-data = ["trend1", "trend2", "trend3"]
-print("Total trends:", len(data))
+file = "cleaned_data.csv"
+
+total = 0
+category_count = {}
+
+with open(file, "r", encoding="utf-8") as f:
+    reader = csv.DictReader(f)
+    
+    for row in reader:
+        total += 1
+        
+        cat = row["category"]
+        if cat in category_count:
+            category_count[cat] += 1
+        else:
+            category_count[cat] = 1
+
+print("Total stories:", total)
+print("Stories per category:")
+
+for k, v in category_count.items():
+    print(k, ":", v)
