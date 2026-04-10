ee4e775500e7d723351fb555e70fdd76ef5710b6
SYEDIBRAHIM21 <127423728+SYEDIBRAHIM21@users.noreply.github.com>
2026-04-10 22:27:23 +0530

    Update Task4_data_visualization.py

--- Task4_data_visualization.py
+++ Task4_data_visualization.py
@@ -1,6 +1,28 @@
-# Task 4: Data Visualization
+import csv
+import matplotlib.pyplot as plt
 
-data = ["trend1", "trend2", "trend3"]
+file = "cleaned_data.csv"
 
-for d in data:
-    print("*", d)
+category_count = {}
+
+with open(file, "r", encoding="utf-8") as f:
+    reader = csv.DictReader(f)
+    
+    for row in reader:
+        cat = row["category"]
+        if cat in category_count:
+            category_count[cat] += 1
+        else:
+            category_count[cat] = 1
+
+# Prepare data
+categories = list(category_count.keys())
+values = list(category_count.values())
+
+# Plot
+plt.bar(categories, values)
+plt.xlabel("Category")
+plt.ylabel("Number of Stories")
+plt.title("Trending Stories by Category")
+
+plt.show()
